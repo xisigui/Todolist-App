6e84162d37b3988f58311aff05a30bac99f5f7d3
xisigui <nagato.leet@gmail.com>
2025-05-30 23:30:59 +0800

    feat: Added login function

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,20 +1,80 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { toast } from "sonner";
 import LoginForm from "@/components/LoginForm";
 import TodoDashboard from "@/components/TodoDashboard";
+import { getAuthToken } from "@/lib/utils";
 
 export default function Home() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [currentUser, setCurrentUser] = useState<string>("");
+  const token = getAuthToken();
 
-  const handleLogin = (username: string, password: string) => {
-    setCurrentUser(username);
-    setIsLoggedIn(true);
+  useEffect(() => {
+    const username = localStorage.getItem("username");
+    if (token && username) {
+      setCurrentUser(username);
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogin = async (username: string, password: string) => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/auth/login/`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        }
+      );
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || "Login failed");
+      }
+
+      const data = await response.json();
+
+      setCurrentUser(data.username);
+      setIsLoggedIn(true);
+
+      if (data.token) {
+        localStorage.setItem("authToken", data.token);
+        localStorage.setItem("username", data.username);
+      }
+    } catch (error) {
+      console.error("Login error:", error);
+      toast.error(error.message);
+    }
   };
 
   const handleLogout = async () => {
-    setCurrentUser("");
-    setIsLoggedIn(false);
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/auth/logout/`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        }
+      );
+      const res = await response.json();
+
+      if (response.ok) {
+        setCurrentUser("");
+        setIsLoggedIn(false);
+        localStorage.removeItem("authToken");
+        localStorage.removeItem("username");
+        toast(res.message);
+      }
+    } catch (error) {
+      console.error("Logout error:", error);
+      toast.error(error.message);
+    }
   };
 
   return (
